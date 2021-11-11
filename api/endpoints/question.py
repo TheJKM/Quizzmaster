0d0352e3b73c9a6ed6d089a9e08bb9724e6c5868
Johannes Kreutz <jkm@jkm-marburg.de>
2021-11-11 01:20:54 +0100

    Fix filename building

--- api/endpoints/question.py
+++ api/endpoints/question.py
@@ -382,6 +382,6 @@ def externalGrading(id):
             points = 0.0 if answer.points is None else answer.points
             csv += str(answer.id) + ";" + str(answer.teamId) + ";" + answer.value + ";" + str(points) + "\n"
         dbSession.close()
-        return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=question_" + question.displayId + "_answers.csv"}), 200
+        return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=question_" + str(question.displayId) + "_answers.csv"}), 200
     elif request.method == "POST":
         return "", 501
