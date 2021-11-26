8590ba2350b73816730f1a716288ea1b08f72b29
Johannes Kreutz <jkm@jkm-marburg.de>
2021-11-26 01:19:49 +0100

    Fix teams view

--- api/endpoints/team.py
+++ api/endpoints/team.py
@@ -63,7 +63,7 @@ def getTeams():
             "name": t.name,
             "information": json.loads(t.information),
             "memberCount": memberCount,
-            "captainEmail": captain.email if captain.email is not None else "-",
+            "captainEmail": captain.email if "email" in captain else "-",
             "displayId": t.displayId
         })
     dbSession.close()
